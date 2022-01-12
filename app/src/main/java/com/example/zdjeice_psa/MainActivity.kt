a3132808e00016b9d26428fce854f36714efdbba
Sobota1337 <janeksob2005@gmail.com>
2022-01-12 10:53:38 +0100

    Pies

---
+++ app/src/main/java/com/example/zdjeice_psa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zdjeice_psa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
